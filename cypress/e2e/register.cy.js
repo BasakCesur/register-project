19e9bd5ea4c82b9c18db3d031f58c42f9d82f372
Başak Özenalp Cesur <basak.ozenalp@gmail.com>
2025-03-17 15:49:11 +0300

    first commit

---
+++ cypress/e2e/register.cy.js
@@ -0,0 +1,47 @@
+import { errorMessages } from "../../src/components/Register";
+describe("Register Page", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:5175/");
+  });
+  describe("Error Message", () => {
+  it('name input throws error for 2 chars', () => {
+    //cy.visit('http://localhost:5175/');
+    cy.get('[data-cy=ad-input]').type('em');
+    cy.contains(errorMessages.ad);
+  })
+   it('Surname input throws error for 2 chars', () => {
+   //cy.visit('http://localhost:5175/');
+    cy.get('[data-cy=soyad-input]').type('şa');
+    cy.contains(errorMessages.soyad);
+  })
+  it('Email input throws error for 2 chars', () => {
+    //cy.visit('http://localhost:5175/');
+    cy.get('[data-cy=email-input]').type('emre@wit');
+    cy.contains(errorMessages.email);
+  })
+  it('Password input throws error for 2 chars', () => {
+    //cy.visit('http://localhost:5175/');
+    cy.get('[data-cy=password-input]').type('1234');
+    cy.contains(errorMessages.password);
+  })
+  it('Button is disabled for unvalidated inputs', () => {
+    //cy.visit('http://localhost:5175/');
+    cy.get('[data-cy=password-input]').type('1234');
+    cy.get('[data-cy=buton-input]').should('be.disabled');
+  })
+  
+})
+describe(" Form inputs validated", () => {
+  it('button enabled for validated inputs', () => {
+    //cy.visit('http://localhost:5175/');
+    cy.get('[data-cy=ad-input]').type('Emre');
+    cy.get('[data-cy=soyad-input]').type('Şahiner');
+    cy.get('[data-cy=email-input]').type('emre@wit.com');
+    cy.get('[data-cy=password-input]').type('1234Aa**');
+    cy.get('[data-cy=buton-input]').should('not.be.disabled');
+    
+  })
+   
+  
+})
+})
